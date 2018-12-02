5a35facee9987fb78b42c15a1586436072af6696
deviantfero <fmorataya.04@gmail.com>
2018-12-02 01:27:31 -0600

    update for t440p

--- scripts/dock-dual.sh
+++ scripts/dock-dual.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 setxkbmap -option caps:escape && \
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --primary --mode 1920x1200 --pos 1366x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x432 --rotate normal --output VGA1 --off
+xrandr --output VGA-1 --mode 1920x1200 --pos 1366x0 --rotate normal --output LVDS-1 --mode 1366x768 --pos 0x432 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
--- scripts/dock-single.sh
+++ scripts/dock-single.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 setxkbmap -option caps:escape && \
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off  --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --auto
