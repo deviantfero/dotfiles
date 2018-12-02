ee8d525e009486838e333ca5391c65c21363f7d1
deviantfero <fmorataya.04@gmail.com>
2018-12-02 01:28:12 -0600

    update for t440p

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q polybar;
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 2; done
+while pgrep -x polybar; do sleep 0.01; done
 
 # Launch bar1 and bar2
-DISPLAY1="$(xrandr -q | grep 'LVDS[0-9]' | cut -d ' ' -f1)"
+DISPLAY1="$(xrandr -q | grep -E 'eDP-?[0-9]' | cut -d ' ' -f1)"
 [[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar wpgtk &
 
-DISPLAY2="$(xrandr -q | grep -E '(HDMI[0-9] connected|VGA1 connected)' | cut -d ' ' -f1)"
+DISPLAY2="$(xrandr -q | grep -E 'DP[1-9]-?[0-9] connected' | cut -d ' ' -f1)"
 [[ ! -z $DISPLAY2 ]] && MONITOR="$DISPLAY2" polybar wpgtk &
 
-[[ -z "$DISPLAY1" ]] && polybar wpgtk &
+DISPLAY3="$(xrandr -q | grep -E 'VGA-?1 connected' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY3 ]] && MONITOR="$DISPLAY3" polybar wpgtk &
